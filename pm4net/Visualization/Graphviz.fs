a5c126f60c99857c5a77a1a2c15ec40115ffd274
Johannes Mols <johannes.mols@googlemail.com>
2023-01-16 22:30:21 +0100

    Started ordering graph into subgraphs by namespace

--- pm4net/Visualization/Graphviz.fs
+++ pm4net/Visualization/Graphviz.fs
@@ -4,6 +4,7 @@ open pm4net.Types
 open DotNetGraph
 open DotNetGraph.Node
 open DotNetGraph.Edge
+open DotNetGraph.SubGraph
 open DotNetGraph.Attributes
 open DotNetGraph.Extensions
 
@@ -19,27 +20,61 @@ module Graphviz =
 
         let graph = DotGraph("DFG", true)
 
-        let nodes =
+        let eventNodesByNameSpace =
             ocdfg.Nodes
-            |> List.map (fun n ->
-                match n with
-                | EventNode n ->
-                    let node = DotNode(n.Name)
+            |> List.choose (fun n -> match n with | EventNode n -> Some n | _ -> None)
+            |> List.groupBy (fun n -> n.Namespace)
+
+        let namespaceSubGraphs =
+            eventNodesByNameSpace
+            |> List.mapi (fun i (ns, nodes) ->
+                let subGraph = DotSubGraph($"cluster_{i}")
+                nodes
+                |> List.map (fun n ->
+                    let node = DotNode($"{n.Name}_{ns}")
                     node.SetCustomAttribute("label", $"<<B>{n.Name}</B><BR/>{n.Frequency}>") |> ignore
                     node.Shape <- DotNodeShapeAttribute DotNodeShape.Rectangle
+                    node.Style <- DotNodeStyleAttribute DotNodeStyle.Filled
+                    node.FillColor <-
+                        match n.Level with
+                        | Debug | Verbose -> DotFillColorAttribute System.Drawing.Color.White
+                        | Information -> DotFillColorAttribute System.Drawing.Color.LightGray
+                        | Warning -> DotFillColorAttribute System.Drawing.Color.Orange
+                        | Error -> DotFillColorAttribute System.Drawing.Color.Red
+                        | Fatal -> DotFillColorAttribute System.Drawing.Color.DarkRed
+                        | _ -> DotFillColorAttribute System.Drawing.Color.White
+                    node.FontColor <-
+                        match n.Level with
+                        | Debug | Verbose -> DotFontColorAttribute System.Drawing.Color.Black
+                        | Information -> DotFontColorAttribute System.Drawing.Color.Black
+                        | Warning -> DotFontColorAttribute System.Drawing.Color.Black
+                        | Error -> DotFontColorAttribute System.Drawing.Color.White
+                        | Fatal -> DotFontColorAttribute System.Drawing.Color.White
+                        | _ -> DotFontColorAttribute System.Drawing.Color.Black
                     node
+                )
+                |> List.iter (fun n -> subGraph.Elements.Add n)
+                subGraph.Label <- ns
+                subGraph
+            )
+        namespaceSubGraphs |> List.iter (fun n -> graph.Elements.Add n)
+
+        let startEndNodes =
+            ocdfg.Nodes
+            |> List.choose (fun n ->
+                match n with
                 | StartNode n ->
                     let node = DotNode(StartNode n |> nodeName)
                     node.Label <- n
                     node.Shape <- DotNodeShapeAttribute DotNodeShape.Ellipse
-                    node
+                    Some node
                 | EndNode n ->
                     let node = DotNode(EndNode n |> nodeName)
                     node.Label <- n
                     node.Shape <- DotNodeShapeAttribute DotNodeShape.Underline
-                    node
-            )
-        nodes |> List.iter (fun n -> graph.Elements.Add n)
+                    Some node
+                | _ -> None)
+        startEndNodes |> List.iter (fun n -> graph.Elements.Add n)
 
         let edges =
             ocdfg.Edges
