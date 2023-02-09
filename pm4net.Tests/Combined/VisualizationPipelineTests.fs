b7c1f26a80f7202d4bd6d27324fd6f6997a98a8b
Johannes Mols <johannes.mols@googlemail.com>
2023-02-09 15:53:15 +0100

    Updated OCEL package

--- pm4net.Tests/Combined/VisualizationPipelineTests.fs
+++ pm4net.Tests/Combined/VisualizationPipelineTests.fs
@@ -12,7 +12,7 @@ module VisualizationPipelineTests =
     [<Fact>]
     let ``Can discover DFG from sample log and generate DOT graph`` () =
         let json = File.ReadAllText("minimal.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let dfg = Discovery.Ocel.OcelDfg.Discover(0, 0, 0, ["customer"; "item"; "order"; "package"; "product"], log)
         let dot = Graphviz.OcDfg2Dot dfg false
         dot |> String.IsNullOrWhiteSpace |> Assert.False
@@ -20,7 +20,7 @@ module VisualizationPipelineTests =
     [<Fact>]
     let ``Can discover DFG from 'Github pm4py' log and generate DOT graph`` () =
         let json = File.ReadAllText("github_pm4py.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let dfg = Discovery.Ocel.OcelDfg.Discover(5, 5, 5, ["case:concept:name"], log)
         let dot = Graphviz.OcDfg2Dot dfg false
         dot |> String.IsNullOrWhiteSpace |> Assert.False
@@ -28,7 +28,7 @@ module VisualizationPipelineTests =
     [<Fact>]
     let ``Can discover DFG from 'blazor-logs' log and generate DOT graph`` () =
         let json = File.ReadAllText("blazor-logs.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let log = log.MergeDuplicateObjects()
         let dfg = Discovery.Ocel.OcelDfg.Discover(0, 0, 0, ["CorrelationId"; "StartDate"; "Now"; "Incremented"], log)
         let dot = Graphviz.OcDfg2Dot dfg false
@@ -37,7 +37,7 @@ module VisualizationPipelineTests =
     [<Fact>]
     let ``Can discover DFG from 'blazor-logs' log and generate DOT graph with namespace clustering`` () =
         let json = File.ReadAllText("blazor-logs.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let log = log.MergeDuplicateObjects()
         let dfg = Discovery.Ocel.OcelDfg.Discover(0, 0, 0, ["CorrelationId"; "StartDate"; "Now"; "Incremented"], log)
         let dot = Graphviz.OcDfg2Dot dfg true
--- pm4net.Tests/Discovery/OcelDfgTests.fs
+++ pm4net.Tests/Discovery/OcelDfgTests.fs
@@ -11,21 +11,21 @@ module OcelDfgTests =
     [<Fact>]
     let ``Can discover DFG from sample log`` () =
         let json = File.ReadAllText("minimal.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let dfg = Discovery.Ocel.OcelDfg.Discover(0, 0, 0, ["customer"; "item"; "order"; "package"; "product"], log)
         true
 
     [<Fact>]
     let ``Can discover DFG from 'Github pm4py' log`` () =
         let json = File.ReadAllText("github_pm4py.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let dfg = Discovery.Ocel.OcelDfg.Discover(5, 5, 5, ["case:concept:name"; "org:resource"; "case:repo"], log)
         true
 
     [<Fact>]
     let ``Can discover DFG from 'blazor-logs' log`` () =
         let json = File.ReadAllText("blazor-logs.jsonocel")
-        let log = OCEL.OcelJson.deserialize json
+        let log = OCEL.OcelJson.deserialize true json
         let log = log.MergeDuplicateObjects()
         let dfg = Discovery.Ocel.OcelDfg.Discover(0, 0, 0, ["CorrelationId"; "Incremented"; "Now"; "StartDate"], log)
         true
